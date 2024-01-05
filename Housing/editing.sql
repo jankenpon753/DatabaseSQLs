304455e9bb02e3690c45e7ddab36bac910ce33a5
jkp_0 <jankenpon753@gmail.com>
2024-01-05 21:17:19 +0600

    creat check 1

--- Housing/editing.sql
+++ Housing/editing.sql
@@ -4,3 +4,5 @@ SET
     Address = 'Flat 601, Building 5, XYZ Housing Society'
 WHERE
     Name = 'Ethan Miller';
+
+DROP DATABASE IF EXISTS HousingSociety;
--- Housing/housingCreate.sql
+++ Housing/housingCreate.sql
@@ -1,73 +1,65 @@
 CREATE DATABASE IF NOT EXISTS HousingSociety;
 
 CREATE TABLE OwnerInfo (
-    OwnerID INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(255),
-    Age INT,
-    PersonalIdentification VARCHAR(9),
-    Phone VARCHAR(11),
-    Email VARCHAR(255),
-    Address VARCHAR(255)
-);
-
-CREATE TABLE ManagerInfo (
-    ManagerID INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(255),
-    Age INT,
-    PersonalID VARCHAR(9),
-    Phone VARCHAR(11),
-    Email VARCHAR(255),
-    Address VARCHAR(255)
+    ownerId INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    age INT,
+    personalIdentification VARCHAR(9),
+    phone VARCHAR(11),
+    email VARCHAR(100),
+    address VARCHAR(255)
 );
 
 CREATE TABLE BuildingInfo (
-    BuildingID INT AUTO_INCREMENT PRIMARY KEY,
-    Floors INT,
-    Flats INT,
-    OwnerID INT,
-    ManagerID INT,
-    Address VARCHAR(255),
-    FOREIGN KEY (OwnerID) REFERENCES OwnerInfo(OwnerID),
-    FOREIGN KEY (ManagerID) REFERENCES ManagerInfo(ManagerID)
+    buildingId INT AUTO_INCREMENT PRIMARY KEY,
+    floors INT,
+    flats INT,
+    ownerId INT,
+    managerId INT,
+    address VARCHAR(255),
+    FOREIGN KEY (ownerId) REFERENCES OwnerInfo(ownerId),
+    FOREIGN KEY (managerId) REFERENCES StaffInfo(staffId)
 );
 
 CREATE TABLE FlatInfo (
-    FlatID INT AUTO_INCREMENT PRIMARY KEY,
-    BuildingID INT,
-    Rooms INT,
-    Rent INT,
-    FlatDescription VARCHAR(255),
-    FOREIGN KEY (BuildingID) REFERENCES BuildingInfo(BuildingID)
+    flatId INT AUTO_INCREMENT PRIMARY KEY,
+    buildingId INT,
+    rooms INT,
+    rent INT,
+    FOREIGN KEY (buildingId) REFERENCES BuildingInfo(buildingId)
 );
 
 CREATE TABLE TenantInfo (
-    TenantID INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(255),
-    Age INT,
-    PersonalIDNumber VARCHAR(16),
-    Phone VARCHAR(11),
-    Email VARCHAR(255),
-    FlatID INT,
-    IsPrimary ENUM('Yes', 'No'),
-    FOREIGN KEY (FlatID) REFERENCES FlatInfo(FlatID)
+    tenantId INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    age INT,
+    personalIdentification VARCHAR(16),
+    phone VARCHAR(11),
+    email VARCHAR(100),
+    flatId INT,
+    isPrimary ENUM('Yes', 'No'),
+    FOREIGN KEY (flatId) REFERENCES FlatInfo(flatId)
 );
 
 CREATE TABLE StaffInfo(
-    StaffID INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(255),
-    Age INT,
-    PersonalIDNumber VARCHAR(9),
-    Phone VARCHAR(11),
-    Email VARCHAR(255),
-    BuildingID INT,
-    JobTitle VARCHAR(100),
-    Salary INT,
-    FOREIGN KEY (BuildingID) REFERENCES BuildingInfo(BuildingID)
+    staffId INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    age INT,
+    personalIdentification VARCHAR(9),
+    phone VARCHAR(11),
+    email VARCHAR(100),
+    buildingId INT,
+    jobTitle VARCHAR(100),
+    salary INT,
+    FOREIGN KEY (buildingId) REFERENCES BuildingInfo(buildingId)
 );
 
 CREATE TABLE RentInfo(
-    FlatID INT,
-    Rent INT,
-    Due VARCHAR(255),
-    FOREIGN KEY (FlatID) REFERENCES FlatInfo(FlatID)
+    flatId INT,
+    rent INT,
+    due VARCHAR(20),
+    FOREIGN KEY (flatId) REFERENCES FlatInfo(flatId)
 );
